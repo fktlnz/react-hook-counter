17d7c7cff5e6db3cd7f20f6fb80c534de4503d0d
fktlnz <fktlnz@gmail.com>
2022-02-25 00:10:04 +0900

    add useCounter.tsx ; mod style

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,55 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import useCounter from './useCounter'
+
+const EndDom:React.VFC<{isShow:boolean}>= (props) => {
+  if(props.isShow) { return <p className="color-red">カウント終了！</p> }
+  return <></>
+}
 
 function App() {
+  const {count, isTimerEnd, initCount, startCount} = useCounter();
+  const [startTime, setStartTime] = useState<number>(0);
+  const [endTime, setEndTime] = useState<number>(0);
+
+  const start = (startTime:number, endTime:number) => {
+    const direction = (startTime>endTime) ? "down" : "up";
+    const upLimit = (direction==="down") ? startTime : endTime;
+    const downLimit = (direction==="down") ? endTime : startTime;
+
+    startCount(upLimit, downLimit, direction)
+  }
+
+  const reset = () => {
+    initCount();
+  }
+
+  const handleChangeStartTime = (e:React.ChangeEvent<HTMLInputElement>) => {
+    setStartTime(parseInt(e.target.value));
+  }
+
+  const handleChangeEndTime = (e:React.ChangeEvent<HTMLInputElement>) => {
+    setEndTime(parseInt(e.target.value));
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <main>
+        <section className='counter-input mb10'>
+          <input onChange={(e)=>handleChangeStartTime(e)} type="text" placeholder='開始時間(sec)'/>
+          <input onChange={(e)=>handleChangeEndTime(e)} type="text" placeholder='終了時間(sec)'/>
+        </section>
+        <section className='counter-button'>
+          <button onClick={()=>start(startTime, endTime)} className='button mb10'>START</button>
+          <button onClick={()=>reset()} className='button mb10' disabled={(isTimerEnd===true) ? false : true}>RESET</button>
+        </section>
+        <section className={(isTimerEnd===true) ? 'counter-value color-red' : 'counter-value'}>
+          {count}
+        </section>
+        <EndDom isShow={isTimerEnd===true}/>
+      </main>
     </div>
   );
 }
